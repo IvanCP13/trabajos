ebbd9e90dbb23f1606b09e37831a202c95c66875
Ivan Caceres <ivancds0151234@hotmail.com>
2022-06-20 15:54:22 -0700

    first commit

---
+++ auto.js
@@ -0,0 +1,7 @@
+let autos = (auto1, auto2)
+
+let auto1 = [ marca, modelo, precio, km, color, cuotas, anio, patente, vendido]
+
+let auto2 = [ marca, modelo, precio, km, color, cuotas, anio, patente, vendido]
+
+console.log(auto1)
